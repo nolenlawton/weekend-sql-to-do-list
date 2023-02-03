d25d55f10be478ffabf816dffe0015e34b655cf7
Nolen Lawton <mnlawton@icloud.com>
2023-02-03 12:50:03 -0600

    connected buttons to functions with event listeners

--- server/public/client.js
+++ server/public/client.js
@@ -4,5 +4,22 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQuery')
+
+    $('#addTaskButton').on('click', addTask)
+
+    $(document).on('click', '.completeButton', completeTask)
+    $(document).on('click', '.deleteButton', deleteTask)
+}
+
+function addTask () {
+    console.log('task was added')
+}
+
+function completeTask() {
+    console.log('task was completed')
+}
+
+function deleteTask() {
+    console.log('task was deleted')
 }
 
